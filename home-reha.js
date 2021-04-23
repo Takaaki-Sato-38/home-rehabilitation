42ab0dd142d5926473231fbf5b98af6d8aa43a36
Takaaki Sato <sabamaruha@gmail.com>
2021-04-23 23:16:54 +0900

    header-tb 作成

---
+++ home-reha.js
@@ -0,0 +1,9 @@
+$(".openbtn1").click(function () {
+    $(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
